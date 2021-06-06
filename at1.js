195b9c811457e08be05dfc67f0862e7db955c4b9
Isabella <dorighetta007@gmail.com>
2021-06-06 17:07:03 -0300

    First Commit

---
+++ at1.js
@@ -0,0 +1,10 @@
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+)
+
+listaDeDestinos.push(`Curitiba`) //adicionando um item no final da lista
+console.log(listaDeDestinos)
+listaDeDestinos.splice(1,2) //removendo elemento de um indice especifico e qtd
+console.log(listaDeDestinos)
